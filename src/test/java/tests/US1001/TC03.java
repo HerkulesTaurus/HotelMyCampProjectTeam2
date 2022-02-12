8e6049969bd02a112eb8986a1242d6a8f1b3b6cb
seyfullah merdan <merdanseyfullah33@gmail.com>
2022-02-12 16:02:23 +0100

    TestBase ve herbir US için Pages classları eklendi

--- src/test/java/tests/US1001/TC03.java
+++ src/test/java/tests/US1001/TC03.java
@@ -1,4 +1,25 @@
 package tests.US1001;
 
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
 public class TC03 {
+
+    @Test
+    public void test01(){
+
+        Driver.getDriver().get("https://www.amazon.com");
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
